8d6282361a6f302131e6a53a3a82ab1a1c314f6f
Eduardo Mont Alverne Lopes <montalverne.edu@gmail.com>
2024-12-05 23:24:44 +0000

    inclusão de título e autora da citação.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>“Sou livre neste mundo e posso abrir qualquer caminho, mas escolho, porque quero, toda vereda que me leve ao estado de amor. O amor, o fogo mais antigo. A única força que dissolve e recria o tempo.”</h1>
+    return <>
+            <h1>Amor</h1>
+            <p>“Sou livre neste mundo e posso abrir qualquer caminho, mas escolho, porque quero, toda vereda que me leve ao estado de amor. O amor, o fogo mais antigo. A única força que dissolve e recria o tempo.” Socorro Acioli.</p>
+    </>
 }
 
 export default Home;
