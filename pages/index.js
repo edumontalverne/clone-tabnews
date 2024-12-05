53e56cc5251cf5c053c7e93299567b5dc1fca7ce
Eduardo Mont Alverne Lopes <montalverne.edu@gmail.com>
2024-12-05 00:04:21 +0000

    outra alteração na página inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>“Sou livre neste mundo e posso abrir qualquer caminho, mas escolho, porque quero, toda vereda que me leve ao estado de amor. O amor, o fogo mais antigo. A única força que dissolve e recria o tempo.”</h1>
+}
+
+export default Home;
