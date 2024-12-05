223151b5c52fe6b8c6c7db9a5a61b90e82bcb51d
Eduardo Mont Alverne Lopes <montalverne.edu@gmail.com>
2024-12-05 23:44:00 +0000

    divisão do texto em parágrafos.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 function Home() {
     return <>
             <h1>Amor</h1>
-            <p>“Sou livre neste mundo e posso abrir qualquer caminho, mas escolho, porque quero, toda vereda que me leve ao estado de amor. O amor, o fogo mais antigo. A única força que dissolve e recria o tempo.” Socorro Acioli.</p>
+            <p>“Sou livre neste mundo e posso abrir qualquer caminho, mas escolho, porque quero, toda vereda que me leve ao estado de amor.</p>
+            <p>O amor, o fogo mais antigo. A única força que dissolve e recria o tempo.”</p>
+            <p> Socorro Acioli.</p>
     </>
 }
 
