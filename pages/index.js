b9f6c3e7def27cf9ebd7a8334d6c368235df7198
Eduardo Lopes <montalverne.edu@gmail.com>
2024-12-08 16:17:30 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 function Home() {
-    return <>
-            <h1>Amor</h1>
-            <p>“Sou livre neste mundo e posso abrir qualquer caminho, mas escolho, porque quero, toda vereda que me leve ao estado de amor.</p>
-            <p>O amor, o fogo mais antigo. A única força que dissolve e recria o tempo.”</p>
-            <p> Socorro Acioli.</p>
+  return (
+    <>
+      <h1>Amor</h1>
+      <p>
+        “Sou livre neste mundo e posso abrir qualquer caminho, mas escolho,
+        porque quero, toda vereda que me leve ao estado de amor.
+      </p>
+      <p>
+        O amor, o fogo mais antigo. A única força que dissolve e recria o
+        tempo.”
+      </p>
+      <p>Socorro Acioli.</p>
     </>
+  );
 }
 
 export default Home;
