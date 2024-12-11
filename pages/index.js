aa7f64958668d230511596348c336bd787664704
Eduardo Lopes <montalverne.edu@gmail.com>
2024-12-11 19:37:19 -0300

    duplica parágrafos para testar continuos deploy

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,14 @@ function Home() {
         O amor, o fogo mais antigo. A única força que dissolve e recria o
         tempo.”
       </p>
+      <p>
+        “Sou livre neste mundo e posso abrir qualquer caminho, mas escolho,
+        porque quero, toda vereda que me leve ao estado de amor.
+      </p>
+      <p>
+        O amor, o fogo mais antigo. A única força que dissolve e recria o
+        tempo.”
+      </p>
       <p>Socorro Acioli.</p>
     </>
   );
